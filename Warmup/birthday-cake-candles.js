89add54f87bc868c1c57f74fe2073885e4c5cb70
Levi <levi.ciarrocchi@gmail.com>
2022-12-14 02:18:09 -0300

    refactor

--- Warmup/birthday-cake-candles.js
+++ Warmup/birthday-cake-candles.js
@@ -2,10 +2,7 @@
 
 function birthdayCakeCandles(candles) {
   const max = Math.max(...candles);
-
-  const maxNumberTimes = candles.filter((a) => a === max);
-
-  return maxNumberTimes.length;
+  return candles.filter((a) => a === max).length;
 }
 
 console.log(birthdayCakeCandles([3, 2, 1, 3]));
